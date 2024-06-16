85b7722b4ad5b1ae366430756006e4a21d2d430c
arturbaccarin <baccarin.artur@gmail.com>
2024-06-16 08:18:30 -0300

    ajuste no main

--- design-patterns/src/criacao/factorymethod/exercicio2/Main.java
+++ design-patterns/src/criacao/factorymethod/exercicio2/Main.java
@@ -5,26 +5,26 @@ public class Main {
     public static void main(String[] args) {
         LogCreator creator;
         Log log;
-        String mensagem = "Hello world!";
-
-        String formato = "console";
+        String formato = "arquivo";
 
         switch (formato) {
             case "arquivo":
                 creator = new LogArquivoCreator();
                 log = creator.criarLog();
-                log.notificar(mensagem);
                 break;
 
             case "console":
                 creator = new LogConsoleCreator();
                 log = creator.criarLog();
-                log.notificar(mensagem);
                 break;
 
             default:
                 System.out.println("valor inválido");
-                break;
+                return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            log.notificar(Integer.toString(i));
         }
     }
 }
